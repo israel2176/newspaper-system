cd1f414dce03923ed7261aefaf266a5b0123fa36
israel2176 <w0527672176@gmail.com>
2026-05-12 12:07:11 +0300

    update: newspaper name 'עמנואל שלי', subdomain emanuel-sheli.israelcodes.ovh

--- docs/js/config.js
+++ docs/js/config.js
@@ -3,16 +3,14 @@
 (function () {
   // When served directly from the VPS (israelcodes.ovh:8080) use relative paths.
   // When served from GitHub Pages use the full VPS URL.
-  const onVPS = window.location.hostname === "israelcodes.ovh";
+  const onVPS = window.location.hostname === "emanuel-sheli.israelcodes.ovh";
 
   window.NEWSPAPER_CONFIG = {
     manifestUrl: onVPS
       ? "/newspaper/manifest.json"
-      : "http://israelcodes.ovh:8080/newspaper/manifest.json",
+      : "http://emanuel-sheli.israelcodes.ovh:8080/newspaper/manifest.json",
 
-    // storageBase is prepended to issue.path / issue.thumb from the manifest.
-    // Empty string = same origin (works when served from VPS directly).
-    storageBase: onVPS ? "" : "http://israelcodes.ovh:8080",
+    storageBase: onVPS ? "" : "http://emanuel-sheli.israelcodes.ovh:8080",
 
     preloadPages: 6,
     readAheadPages: 8,
